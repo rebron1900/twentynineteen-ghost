338bb8388f787ea236fd6840022705b13bac4d0a
rebron1900 <me@1900.live>
2023-05-25 14:38:55 +0800

    js/post/post: 完善评论人员显示

--- src/js/post/post.js
+++ src/js/post/post.js
@@ -1,37 +1,47 @@
 // Ship JS only active on post pages for better performance
 twikoo.init({
   envId: 'https://comment.1900.live', el: '.tk-comments', onCommentLoaded: function () {
-    twikoo.getRecentComments({
-      envId: 'https://comment.1900.live', // 环境 ID
-      // region: 'ap-guangzhou', // 环境地域，默认为 ap-shanghai，如果您的环境地域不是上海，需传此参数
-      pageSize: 5, // 获取多少条，默认：10，最大：100
-      includeReply: false // 是否包括最新回复，默认：false
-    }).then(function (res) {
-      console.log(res);
-      res.forEach(e => {
-        const avatarlist = document.querySelector('.discussion-avatar-list');
-        const li = document.querySelector('.discussion-avatar-list li').cloneNode(true);
-        li.getElementsByTagName('img')[0].setAttribute('src', e.avatar)
-        avatarlist.appendChild(li)
-      });
-      document.querySelector('.discussion-avatar-list li').remove()
-    }).catch(function (err) {
-      // 发生错误
-      console.error(err);
+    // twikoo.getRecentComments({
+    //   envId: this.envId, // 环境 ID
+    //   // region: 'ap-guangzhou', // 环境地域，默认为 ap-shanghai，如果您的环境地域不是上海，需传此参数
+    //   pageSize: 5, // 获取多少条，默认：10，最大：100
+    //   includeReply: false // 是否包括最新回复，默认：false
+    // }).then(function (res) {
+    //   console.log(res);
+
+    //   res.forEach(e => {
+    //     const avatarlist = document.querySelector('.discussion-avatar-list');
+    //     const li = document.querySelector('.discussion-avatar-list li').cloneNode(true);
+    //     li.getElementsByTagName('img')[0].setAttribute('src', e.avatar)
+    //     avatarlist.appendChild(li)
+    //   });
+    //   document.querySelector('.discussion-avatar-list li').remove()
+    // }).catch(function (err) {
+    //   // 发生错误
+    //   console.error(err);
+    // });
+
+    const cm = document.querySelectorAll('[class=tk-comment]');
+    const temp = document.querySelector('.discussion-avatar-list li').cloneNode(true);
+    document.querySelector('.discussion-avatar-list li').remove();
+    const list = [];
+
+    cm.forEach((e, i) => {
+      const avatarlist = document.querySelector('.discussion-avatar-list');
+      const av = e.getElementsByTagName('img')[0].getAttribute('src');
+      const ct = e.getElementsByClassName('tk-content')[0].innerText;
+      const li = temp.cloneNode(true);
+      li.getElementsByTagName('img')[0].setAttribute('src', av);
+      li.getElementsByTagName('img')[0].setAttribute('title', ct);
+      avatarlist.appendChild(li)
+      if(i == 10){
+        return;
+      }
     });
 
-    twikoo.getCommentsCount({
-      envId: 'https://comment.1900.live', // 环境 ID
-      // region: 'ap-guangzhou', // 环境地域，默认为 ap-shanghai，如果您的环境地域不是上海，需传此参数
-      urls: [ document.location.pathname ],
-      includeReply: false // 评论数是否包括回复，默认：false
-    }).then(function (res) {
-      console.log(res);
-      document.querySelector('.discussion-meta-info span').innerText = res[0].count + '条评论';
-    }).catch(function (err) {
-      // 发生错误
-      console.error(err);
-    });
+    document.querySelector('.discussion-meta-info span').innerText= document.querySelector('.discussion-meta-info span').innerText.replace('?',cm.length);
+    
+    console.log(list);
   }
 })
 
